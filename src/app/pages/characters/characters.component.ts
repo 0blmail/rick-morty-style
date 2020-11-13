0124af1cb1ba3e9b5fea17f4eca864ab9bd560d7
Liam <liam.ladrouz@gmail.com>
2020-11-13 22:24:44 +0100

    :sparkles: : Display characters cards

--- src/app/pages/characters/characters.component.ts
+++ src/app/pages/characters/characters.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Character } from '../../shared/models/Character';
+import { Frame } from '../../shared/models/Frame';
 import { CharacterService } from '../../shared/services/character.service';
 
 @Component({
@@ -8,13 +10,14 @@ import { CharacterService } from '../../shared/services/character.service';
 })
 export class CharactersComponent implements OnInit {
 
+  characters: Frame<Character>;
+
   constructor(private characterService: CharacterService) {
   }
 
   ngOnInit(): void {
-    this.characterService.getAllCharacters().subscribe((c) => {
-      console.log(c);
-    });
+    this.characterService.getAllCharacters()
+        .subscribe((characters: Frame<Character>) => this.characters = characters);
   }
 
 }
