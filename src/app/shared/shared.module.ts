93ae801696acd1f908b4c9f1753f01431cb51d53
Liam <liam.ladrouz@gmail.com>
2020-11-13 19:25:49 +0100

    :sparkles: : Add not found component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,16 @@
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 import { CharacterService } from './services/character.service';
 import { LocationService } from './services/location.service';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ NotFoundComponent ],
   imports: [
     CommonModule,
+    HttpClientModule,
   ],
   providers: [
     LocationService,
